fd7d7786bed9759357e01a7da4e1800b83fc762f
Adriano de Aguiar <adrianoaguiardez@gmail.com>
2021-03-25 15:36:42 -0400

    herok

--- src/main/java/br/com/simconsult/painel/config/CarregaBaseDados.java
+++ src/main/java/br/com/simconsult/painel/config/CarregaBaseDados.java
@@ -1,19 +1,19 @@
 package br.com.simconsult.painel.config;
 
-import java.util.Collections;
-
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.core.Ordered;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
-
-@CrossOrigin
-@Configuration
-public class CarregaBaseDados {
+//import java.util.Collections;
+//
+//import org.springframework.boot.web.servlet.FilterRegistrationBean;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.core.Ordered;
+//import org.springframework.web.bind.annotation.CrossOrigin;
+//import org.springframework.web.cors.CorsConfiguration;
+//import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+//import org.springframework.web.filter.CorsFilter;
+//
+//@CrossOrigin
+//@Configuration
+//public class CarregaBaseDados {
 
 //	@Autowired
 //	private UserService userService;
@@ -52,25 +52,25 @@ public class CarregaBaseDados {
 //		};
 //	}
 
-	@Bean
-	public FilterRegistrationBean<CorsFilter> corsFilter() {
+//	@Bean
+//	public FilterRegistrationBean<CorsFilter> corsFilter() {
+//
+//		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//		CorsConfiguration config = new CorsConfiguration();
+//		config.setAllowCredentials(true);
+//		config.addAllowedOrigin("*");
+//		config.addAllowedHeader("*");
+//		config.addAllowedMethod("OPTIONS");
+//		config.addAllowedMethod("GET");
+//		config.addAllowedMethod("PUT");
+//		config.addAllowedMethod("POST");
+//		config.addAllowedMethod("DELETE");
+//		config.addAllowedMethod("PATCH");
+//
+//		source.registerCorsConfiguration("/**", config);
+//		final FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<CorsFilter>(new CorsFilter(source));
+//		bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+//		return bean;
+//	}
 
-		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-		CorsConfiguration config = new CorsConfiguration();
-		config.setAllowCredentials(true);
-		config.addAllowedOrigin("*");
-		config.addAllowedHeader("*");
-		config.addAllowedMethod("OPTIONS");
-		config.addAllowedMethod("GET");
-		config.addAllowedMethod("PUT");
-		config.addAllowedMethod("POST");
-		config.addAllowedMethod("DELETE");
-		config.addAllowedMethod("PATCH");
-
-		source.registerCorsConfiguration("/**", config);
-		final FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<CorsFilter>(new CorsFilter(source));
-		bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
-		return bean;
-	}
-
-}
+//}
